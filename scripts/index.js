3f8ff538badfe58d5456574a2936ec04356221d0
sasha <avoriemail@gmail.com>
2024-12-11 11:24:47 +0000

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.144.0/build/three.module.js';
-import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/loaders/OBJLoader.js';
+import { OBJLoader } from '/scripts/objLoader';
 bridge = '/models/bridge.obj'
 
 const scene = new THREE.Scene();
